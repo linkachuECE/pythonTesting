48556f1aa73e91b11648e6a7e92eea8c666dd81b
Ethan <ethangbraun@gmail.com>
2021-06-13 23:00:00 -0500

    minor experiments in pythonTesting

--- pythonTesting.py
+++ pythonTesting.py
@@ -1,14 +1,3 @@
-from DataStructureExamples import HashTable
+from DataStructureExamples import *
 
-H=HashTable()
-H[54]="cat"
-H[26]="dog"
-H[93]="lion"
-H[17]="tiger"
-H[77]="bird"
-H[31]="cow"
-H[44]="goat"
-H[55]="pig"
-H[20]="chicken"
-print(H.slots)
-print(H.data)
+print(shellSort([10,9,8,7,6,5,4,3,2,1]))
