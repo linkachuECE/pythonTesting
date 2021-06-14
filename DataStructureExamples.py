80fad02b80abdd3ff3b9cdc338f23fd5896529ec
Ethan <ethangbraun@gmail.com>
2021-06-13 22:59:27 -0500

    Added sorting algorithms to supplementary file

--- DataStructureExamples.py
+++ DataStructureExamples.py
@@ -341,9 +341,11 @@ class HashTable:
 		self.slots = [None] * self.size
 		self.data = [None] * self.size
 	
-	def put(self,key,data):
+	def put(self,key,data=None):
 		hashvalue = self.hashfunction(key,len(self.slots))
-
+		if data == None:
+			data = key
+		
 		if self.slots[hashvalue] == None:
 			self.slots[hashvalue] = key
 			self.data[hashvalue] = data
@@ -357,11 +359,16 @@ class HashTable:
 				
 				if self.slots[nextslot] == None:
 					self.slots[nextslot] = key
-				else:
 					self.data[nextslot] = data
 					
 	def hashfunction(self,key,size):
-		return key%size
+		if isinstance(key, str):
+			totalOrd = 0
+			for char in key:
+				totalOrd += ord(char)
+			return totalOrd%size
+		else:
+			return key%size
 	
 	def rehash(self,oldhash,size):
 		return (oldhash+1)%size
@@ -381,10 +388,145 @@ class HashTable:
 				position=self.rehash(position, len(self.slots))
 				if position == startslot:
 					stop = True
-		return data
+		return position
 	
 	def __getitem__(self,key):
 		return self.get(key)
 	
 	def __setitem__(self,key,data):
 		self.put(key,data)
+
+def shortBubbleSort(alist):
+	exchanges = True
+	passnum = len(alist)-1
+	while passnum > 0 and exchanges:
+		exchanges = False
+		for i in range(passnum):
+			if alist[i]>alist[i+1]:
+				exchanges = True
+				temp = alist[i]
+				alist[i] = alist[i+1]
+				alist[i+1] = temp
+		passnum = passnum-1
+	return alist
+
+def selectionSort(alist):
+	for fillslot in range(len(alist)-1,0,-1):
+		positionOfMax=0
+		for location in range(1,fillslot+1):
+			if alist[location]>alist[positionOfMax]:
+				positionOfMax = location
+
+		temp = alist[fillslot]
+		alist[fillslot] = alist[positionOfMax]
+		alist[positionOfMax] = temp
+	return alist
+
+def insertionSort(alist):
+	for index in range(1,len(alist)):
+		currentvalue = alist[index]
+		position = index
+
+		while position>0 and alist[position-1]>currentvalue:
+			alist[position]=alist[position-1]
+			position = position-1
+
+		alist[position]=currentvalue
+	return alist
+
+def shellSort(alist):
+	sublistcount = len(alist)//2
+	while sublistcount > 0:
+
+		for startposition in range(sublistcount):
+			gapInsertionSort(alist,startposition,sublistcount)
+
+		print("After increments of size",sublistcount,"The list is",alist)
+
+		sublistcount = sublistcount // 2
+	return alist
+
+def gapInsertionSort(alist,start,gap):
+	for i in range(start+gap,len(alist),gap):
+
+		currentvalue = alist[i]
+		position = i
+
+		while position>=gap and alist[position-gap]>currentvalue:
+			alist[position]=alist[position-gap]
+			position = position-gap
+
+		alist[position]=currentvalue
+
+def mergeSort(alist):
+	print("Splitting ",alist)
+	if len(alist)>1:
+		mid = len(alist)//2
+		lefthalf = alist[:mid]
+		righthalf = alist[mid:]
+
+		mergeSort(lefthalf)
+		mergeSort(righthalf)
+
+		i=0
+		j=0
+		k=0
+		while i < len(lefthalf) and j < len(righthalf):
+			if lefthalf[i] <= righthalf[j]:
+				alist[k]=lefthalf[i]
+				i=i+1
+			else:
+				alist[k]=righthalf[j]
+				j=j+1
+			k=k+1
+
+		while i < len(lefthalf):
+			alist[k]=lefthalf[i]
+			i=i+1	
+			k=k+1
+
+		while j < len(righthalf):
+			alist[k]=righthalf[j]
+			j=j+1
+			k=k+1
+	print("Merging ",alist)
+
+def quickSort(alist):
+	quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+	if first<last:
+
+		splitpoint = partition(alist,first,last)
+
+		quickSortHelper(alist,first,splitpoint-1)
+		quickSortHelper(alist,splitpoint+1,last)
+
+def partition(alist,first,last):
+	pivotvalue = alist[first]
+
+	leftmark = first+1
+	rightmark = last
+
+	done = False
+	while not done:
+
+		while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+			leftmark = leftmark + 1
+
+		while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+			rightmark = rightmark -1
+
+		if rightmark < leftmark:
+			done = True
+		else:
+			temp = alist[leftmark]
+			alist[leftmark] = alist[rightmark]
+			alist[rightmark] = temp
+
+	temp = alist[first]
+	alist[first] = alist[rightmark]
+	alist[rightmark] = temp
+
+	return rightmark
+
