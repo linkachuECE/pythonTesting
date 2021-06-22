e65eb90bb9ca9fdf1eb45ebd128dad46e37c3893
Ethan <ethangbraun@gmail.com>
2021-06-21 23:43:13 -0500

    Added a class for binary trees

--- pythonTesting.py
+++ pythonTesting.py
@@ -3,5 +3,37 @@ from DataStructureExamples import *
 from functionBenchmarkComparison import *
 import random
 
-testList = ListToSort([5,4,3,2,1])
-print(testList.quickSort())
+def buildParseTree(fpexp):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+
+        elif i in ['+', '-', '*', '/']:
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+
+        elif i == ')':
+            currentTree = pStack.pop()
+
+        elif i not in ['+', '-', '*', '/', ')']:
+            try:
+                currentTree.setRootVal(int(i))
+                parent = pStack.pop()
+                currentTree = parent
+
+            except ValueError:
+                raise ValueError("token '{}' is not a valid integer".format(i))
+
+    return eTree
+
+pt = buildParseTree("( ( 10 + 5 ) * 3 )")
