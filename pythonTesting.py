59b2d6d300deca0f5cf7f6cbc3427a70203f8962
Ethan <ethangbraun@gmail.com>
2021-06-23 14:07:16 -0500

    added a binary heap class

--- pythonTesting.py
+++ pythonTesting.py
@@ -3,37 +3,5 @@ from DataStructureExamples import *
 from functionBenchmarkComparison import *
 import random
 
-def buildParseTree(fpexp):
-    fplist = fpexp.split()
-    pStack = Stack()
-    eTree = BinaryTree('')
-    pStack.push(eTree)
-    currentTree = eTree
-
-    for i in fplist:
-        if i == '(':
-            currentTree.insertLeft('')
-            pStack.push(currentTree)
-            currentTree = currentTree.getLeftChild()
-
-        elif i in ['+', '-', '*', '/']:
-            currentTree.setRootVal(i)
-            currentTree.insertRight('')
-            pStack.push(currentTree)
-            currentTree = currentTree.getRightChild()
-
-        elif i == ')':
-            currentTree = pStack.pop()
-
-        elif i not in ['+', '-', '*', '/', ')']:
-            try:
-                currentTree.setRootVal(int(i))
-                parent = pStack.pop()
-                currentTree = parent
-
-            except ValueError:
-                raise ValueError("token '{}' is not a valid integer".format(i))
-
-    return eTree
-
-pt = buildParseTree("( ( 10 + 5 ) * 3 )")
+bh = BinHeap()
+bh.buildHeap([5,4,3,2,1])
