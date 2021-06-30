3d49cb00b84fe40d5e966470856685897ab97315
Ethan <ethangbraun@gmail.com>
2021-06-30 01:37:57 -0500

    Managed to get sympy, a math module, up and running. I'll give it a test run tomorrow

--- pythonTesting.py
+++ pythonTesting.py
@@ -2,12 +2,5 @@ from typing import List
 from DataStructureExamples import *
 from functionBenchmarkComparison import *
 import random
-import 
+import sympy
 
-mytree = BinMaxHeap()
-testList = []
-for i in range(0,31):
-    testList.append(random.randint(0,100))
-
-mytree.buildHeap(testList)
-print(mytree)
